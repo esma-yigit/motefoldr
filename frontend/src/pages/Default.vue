05005d0415b9db123790015dd675b90e7558fbcb
esma-yigit <75517864+esma-yigit@users.noreply.github.com>
2022-06-11 23:29:24 +0300

    dix

--- frontend/src/pages/Default.vue
+++ frontend/src/pages/Default.vue
@@ -2,11 +2,9 @@
   import Header from '@/components/layouts/Header.vue'
   import Footer from '@/components/layouts/Footer.vue'
 </script>
-
 <template>
   <Header />
   <router-view />
   <Footer />
 </template>
-
 <style scoped></style>
